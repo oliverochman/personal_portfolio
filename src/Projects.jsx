2b35a4ae22497b493653c0a9394740e30bbb593d
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-18 14:13:25 +0100

    editing the projects so that they actually show my prjects and then styling it up with some pictures

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ProjectCard from './ProjectCard'
+import ProjectCard from './ProjectCard';
 
 class Projects extends Component {
     state = {
