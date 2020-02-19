839e52b02df8a0c9edecb85fca5d804b54407ded
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-19 08:55:31 +0100

    trying to make the confetti work with hello world, not quite there yet...

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-// import Sprinkler from "./sprinkler";
-// import Sprinkler from '/src/sprinkler.jsx'
 import Confetti from 'react-confetti'
 
-const Sprinkler = () => {
-    return (
+const Hello = () => {
+    return ( 
+      <div className="ui main container">
+        <h1 id="hello">Hello World</h1>
+      </div>
+    );
+};
+const Sparks = () => {
+    return ( 
       <Confetti
         width = {2000} 
         height={1000}
@@ -12,13 +17,4 @@ const Sprinkler = () => {
       />
     )
   }
-
-  export default Sprinkler;
-// const Hello = () => {
-//   return ( 
-//     <div className="ui main container">
-//       <h1 id="hello">Hello Worldz</h1>
-//     </div>
-//   );
-// };
-// export default Hello;
+export default Hello;
