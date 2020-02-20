c85c5b08a08cf6542903a1a90bf9c482c418ed46
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-20 17:13:55 +0100

    trying to do something cool and ended up with a slideshow that looks like a 3yrs old did it

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Slide } from "react-slideshow-image";
 
-const Contact = () => {
 
-    return (
-        <div className='ui main container' >
-            <h1 id='contact-header' className='ui header'>Contact Me</h1>
+const slideImages = [
+    'src/images/linkedin.jpeg',
+    'images/instagram.jpeg',
+    'images/facebook.jpeg'
+  ];
+   
+  const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+    }
+  }
+   
+  const Contact = () => {
+      return (
+        <div className="slide-container">
+          <Slide {...properties}>
+            <div className="each-slide">
+              <div>
+                <span><i class="instagram icon"></i> @emthalen</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+                <span><i class="linkedin icon"></i>Emma-Maria Thalen</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+                <span><i class="facebook square icon"></i>Emma-Maria Thalen</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+                <span><i class="envelope icon"></i>thalen.emma@gmail.com</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+                <span><i class="mobile alternate icon"></i>+46(0)76-105 8101</span>
+              </div>
+            </div>
+          </Slide>
         </div>
-    );
-};
-
-export default Contact;
+      )
+  }
+  
+  export default Contact
