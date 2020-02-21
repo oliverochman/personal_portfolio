559e54687d0b407c462e4e5770b584b26dc2c747
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-21 14:21:05 +0100

    completing the test for the contact page and testing to see that the content changes with the slides

--- cypress/integration/userCanSeeContactInformation.feature.js
+++ cypress/integration/userCanSeeContactInformation.feature.js
@@ -5,10 +5,25 @@ describe('User can see contact information', () => {
     });
 
     it('displays image on the page', () => {
-        cy.get(('.image').should('exist'))
+        cy.get(('#contact-pic')).should('exist')
     });
 
     it('displays slideshow', () => {
-        cy.get()
+        cy.get('.each-slide').should('contain', 'thalen.emma@gmail.com')
+    })
+
+    it('displays second slide', () => {
+        cy.get('.each-slide').wait(5000)
+        cy.get('.each-slide').should('contain', '+46(0)76-105 8101')
+    })
+
+    it('displays third slide', () => {
+        cy.get('.each-slide').wait(5000)
+        cy.get('.each-slide').should('contain', '@emthalen')
+    })
+
+    it('displays forth slide', () => {
+        cy.get('.each-slide').wait(5000)
+        cy.get('.each-slide').should('contain', 'Emma-Maria Thalen')
     })
 })
