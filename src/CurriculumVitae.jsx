434add6c57eb87f3adb07fa3591e5f6f341907c4
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-22 12:41:16 +0100

    finding a way to do my CV as a timeline and importing the data from a json file

--- src/CurriculumVitae.jsx
+++ src/CurriculumVitae.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { Icon } from 'semantic-ui-react';
+import axios from 'axios'
 
-const CV = () => {
-    return (
-        
-    )
+class Cv extends Component {
+    state = {
+        cvExperience: []
+    };
+    componentDidMount() {
+        axios.get('./src/data/experience.json')
+            .then(response => {
+                const cvExperience = response.data
+                this.setState({ cvExperience })
+            })
+    };
+
+    render() {
+        return (
+            <div className='ui main container' >
+                <h1 id='about-header' className='ui header'>My CV</h1>
+                
+            <VerticalTimeline>
+                {this.state.cvExperience.map(experience =>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: 'rgb(255, 255, 255)', color: '#fffff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  rgb(255, 255, 255)' }}
+                    date={experience.date} 
+                    iconStyle={{ background: 'rgb(255, 255, 255)', color: '#fffff'}}
+                    icon={<Icon size='huge' name={experience.icon} />}
+                > 
+                    <h3 className="vertical-timeline-element-title">{experience.title}</h3>
+                    <h4 className="vertical-timeline-element-subtitle">{experience.location}</h4>
+                    <p>
+                    {experience.description}            
+                    </p>
+                </VerticalTimelineElement>
+                )}
+            </VerticalTimeline>
+        </div>
+        );
+    }
 }
 
-export default CV; 
+export default Cv;
