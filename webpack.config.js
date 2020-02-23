c32af0714d878da983c531178fdaf1c5c84baffc
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-23 14:25:06 +0100

    trying to go back to where i was before trying a stupid solution

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
