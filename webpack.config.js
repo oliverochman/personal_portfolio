1d2ee8897fb1cf3e62b0c795e54d5cd1150852c4
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-24 16:37:42 +0100

    Debugs depluyment [WIP]

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
